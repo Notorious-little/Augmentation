2016fbae54e9532c0d7147f74a7c9fc72985edc3
Notorious-little <72048638+Notorious-little@users.noreply.github.com>
2020-10-11 15:56:38 +0200

    Update methods.cpp

--- methods.cpp
+++ methods.cpp
@@ -7,7 +7,7 @@
 #include <cstdio>
 #include <cmath>
 
-cv::Mat autoContrast(const cv::Mat img, const int quantil){
+cv::Mat autoContrast(const cv::Mat &img, const int quantil){
 
     assert(0 <= quantil && quantil <= 100);
 
@@ -115,108 +115,6 @@ return img_autocontr;
 
 
 
-cv::Mat localContours(const cv::Mat input_img){
-
-    cv::Mat output_img( input_img.rows , input_img.cols , CV_8UC1 );
-
-    cv::Mat b1 = input_img.clone();
-
-    b1 = autoContrast(b1, 5);
-
-    cvtColor( input_img , b1 , cv::COLOR_BGR2GRAY );
-    cv::Mat b;
-    b1.convertTo(b, CV_8UC1);
-
-    const int h = input_img.rows;
-    const int w = input_img.cols;
-
-    for (int i = 2; i < h-2; ++i ){
-        for (int j = 2; j < w-2; ++j ){
-
-        double s = 0;
-
-            for(int k = j - 1; k <= j+1; ++k){
-                s += abs (b.at<uchar>(i,k) - b.at<uchar>(i-2,k));
-                s += abs (b.at<uchar>(i+1,k) - b.at<uchar>(i-1,k));
-                s += abs (b.at<uchar>(i+2,k) - b.at<uchar>(i,k));
-            }
-
-            for(int k = i - 1; k <= i+1; ++k){
-                s += abs (b.at<uchar>(k,j) - b.at<uchar>(k,j-2));
-                s += abs (b.at<uchar>(k,j+1) - b.at<uchar>(k,j-1));
-                s += abs (b.at<uchar>(k,j+2) - b.at<uchar>(k,j));
-            }
-
-            for(int k = i - 2; k <= i; ++k){
-                s += abs (b.at<uchar>(k, j-2) - b.at<uchar>(k+2,j));
-                s += abs (b.at<uchar>(k,j-1) - b.at<uchar>(k+2,j+1));
-                s += abs (b.at<uchar>(k,j) - b.at<uchar>(k+2,j+2));
-            }
-
-            for(int k = i; k <= i + 2; ++k){
-                s += abs (b.at<uchar>(k, j-2) - b.at<uchar>(k-2,j));
-                s += abs (b.at<uchar>(k,j-1) - b.at<uchar>(k-2,j+1));
-                s += abs (b.at<uchar>(k,j) - b.at<uchar>(k-2,j+2));
-            }
-
-            s /= 36;
-
-            double s_vert(0), s_hor(0), s_maindig(0), s_subdig(0), s_max(0);
-
-            for(int k = -1; k <= 1; ++k){
-                s_vert += abs(b.at<uchar>(i-1,j+k) - b.at<uchar>(i+1,j+k));
-            }
-
-            for(int k = -1; k <= 1; ++k){
-                s_hor += abs(b.at<uchar>(i+k,j-1) - b.at<uchar>(i+k,j+1));
-            }
-
-            for(int k = 0; k <= 2; ++k){
-                s_maindig += abs(b.at<uchar>(i-2+k,j+k) - b.at<uchar>(i+k,j-2+1));
-            }
-
-            for(int k = 0; k <= 2; ++k){
-                s_subdig += abs(b.at<uchar>(i-2-k,j-k) - b.at<uchar>(i-k,j+2-k));
-            }
-
-
-
-            if ( s_vert >= s_hor && s_vert >= s_maindig && s_vert >= s_subdig){
-                s_max = s_vert;
-                if (s_max > s*3*1.8) {
-                    for(int c =-1 ; c < 1; ++c){
-                      if (s_hor < s_maindig && s_hor < s_subdig)  output_img.at<uchar>(i,j+c) = 254;
-                    }
-                }
-            } else if(s_hor >= s_vert && s_hor >= s_maindig && s_hor >= s_subdig){
-                       s_max = s_hor;
-                       if (s_max > s*3*1.8) {
-                           for(int c =-1 ; c < 1; ++c){
-                            if (s_vert < s_maindig && s_vert < s_subdig) output_img.at<uchar>(i+c,j) = 254;
-                           }
-                       }
-            } else if(s_maindig >= s_hor && s_maindig >= s_vert && s_maindig >= s_subdig){
-                       s_max = s_maindig;
-                       if (s_max > s*3*1.6) {
-                           for(int c =-1 ; c < 2; ++c){
-                               if(s_vert > 1.4*s_subdig && s_hor > 1.4*s_subdig) output_img.at<uchar>(i+c,j+c) = 254;
-                           }
-                       }
-            } else {
-                s_max = s_subdig;
-                if (s_max > s*3*1.6) {
-                    for(int c =-1 ; c < 2; ++c){
-                        if(s_vert > 1.4*s_maindig && s_hor > 1.4*s_maindig) output_img.at<uchar>(i+c,j-c) = 254;
-                    }
-                }
-              }
-        }
-    }
-
-return output_img;
-}
-
-
 
 void labelText(cv::Mat img){
 
@@ -264,9 +162,10 @@ return;
 
 
 
-cv::Mat blurByGaussMatrix(const cv::Mat input_img, const int blurpower){
+cv::Mat blurByGaussMatrix(const cv::Mat &input_img, const int blurpower){
 
     cv::Mat bluring_img = input_img.clone();
+
     const int n = 2*blurpower+1;
     const int h = input_img.rows;
     const int w = input_img.cols;
@@ -279,7 +178,7 @@ cv::Mat blurByGaussMatrix(const cv::Mat input_img, const int blurpower){
         }
     }
 
-    double Matrix[ n*n ] = {0};
+    double* Matrix = new double [n*n];
 
     slidingNormalMatrixCompute( Matrix , blurpower );
 
@@ -306,6 +205,8 @@ return bluring_img;
 }
 
 
+
+
 /* #define RAND_MAX 2147483647*/
 double exp_rand(void){
 
@@ -329,7 +230,9 @@ return 0;
 }
 
 
-cv::Mat gaussNoize( const cv::Mat input_img, const int noize_range){
+
+
+cv::Mat gaussNoize( const cv::Mat &input_img, const int noize_range){
 
         cv::Mat noizing_img = input_img.clone();
 
@@ -358,3 +261,211 @@ cv::Mat gaussNoize( const cv::Mat input_img, const int noize_range){
 return noizing_img;
 }
 
+
+
+
+cv::Mat salt_paperNoize( const cv::Mat &input_img, const int noize_range){
+
+        cv::Mat noizing_img = input_img.clone();
+
+        int w = noizing_img.cols;
+        int h = noizing_img.rows;
+
+        for (int i = 0; i < h; ++i){
+                for (int j = 0; j < w; ++j){
+
+                    int a = rand();
+
+                    if( a < noize_range ){
+                        noizing_img.at<cv::Vec3b>(i,j)[0] = 0;
+                        noizing_img.at<cv::Vec3b>(i,j)[1] = 0;
+                        noizing_img.at<cv::Vec3b>(i,j)[2] = 0;
+                    }
+
+                    if( a > (RAND_MAX - noize_range) ){
+                        noizing_img.at<cv::Vec3b>(i,j)[0] = 255;
+                        noizing_img.at<cv::Vec3b>(i,j)[1] = 255;
+                        noizing_img.at<cv::Vec3b>(i,j)[2] = 255;
+                    }
+
+                }
+        }
+
+return noizing_img;
+}
+
+
+
+
+cv::Mat localContours(const cv::Mat &input_img){
+
+    cv::Mat output_img( input_img.rows , input_img.cols , CV_8UC1 );
+    cv::Mat b1 = input_img.clone();
+    cvtColor( input_img , b1 , cv::COLOR_BGR2GRAY );
+
+    cv::Mat b;
+    b1.convertTo(b, CV_8UC1);
+
+    const double AV = 1.3;
+    const double GK = 1.41;
+    const double G_AV = 5.;
+
+    const int h = input_img.rows;
+    const int w = input_img.cols;
+
+    long double av_diff_vert(0), av_diff_hor(0), av_diff_maindig(0), av_diff_subdig(0);
+
+    for (int i = 2; i < h-2; ++i ){
+
+    long double tmp_diff_vert(0), tmp_diff_hor(0),
+                tmp_diff_maindig(0), tmp_diff_subdig(0);
+
+        for (int j = 2; j < w-2; ++j ){
+
+                tmp_diff_vert += abs(b.at<uchar>(i-1,j) - b.at<uchar>(i+1,j));
+
+                tmp_diff_hor += abs(b.at<uchar>(i,j-1) - b.at<uchar>(i,j+1));
+
+                tmp_diff_maindig += abs(b.at<uchar>(i-1,-1) - b.at<uchar>(i+1,j+1));
+
+                tmp_diff_subdig += abs(b.at<uchar>(i-1,j+1) - b.at<uchar>(i+1,j-1));
+
+        }
+
+        av_diff_vert    += (tmp_diff_vert / w);
+        av_diff_hor     += (tmp_diff_hor / w);
+        av_diff_maindig += (tmp_diff_maindig / w);
+        av_diff_subdig  += (tmp_diff_subdig / w);
+    }
+
+    av_diff_vert    /= h;
+    av_diff_hor     /= h;
+    av_diff_maindig /= h;
+    av_diff_subdig  /= h;
+
+
+
+    for (int i = 2; i < h-2; ++i ){
+        for (int j = 2; j < w-2; ++j ){
+
+        output_img.at<uchar>(i,j) = 0;
+
+        double s = 0;
+
+            for(int k = j - 1; k <= j+1; ++k){
+                s += abs (b.at<uchar>(i,k) - b.at<uchar>(i-2,k))*GK;
+                s += abs (b.at<uchar>(i+1,k) - b.at<uchar>(i-1,k))*GK;
+                s += abs (b.at<uchar>(i+2,k) - b.at<uchar>(i,k))*GK;
+            }
+
+            for(int k = i - 1; k <= i+1; ++k){
+                s += abs (b.at<uchar>(k,j) - b.at<uchar>(k,j-2))*GK;
+                s += abs (b.at<uchar>(k,j+1) - b.at<uchar>(k,j-1))*GK;
+                s += abs (b.at<uchar>(k,j+2) - b.at<uchar>(k,j))*GK;
+            }
+
+            for(int k = i - 2; k <= i; ++k){
+                s += abs (b.at<uchar>(k, j-2) - b.at<uchar>(k+2,j));
+                s += abs (b.at<uchar>(k,j-1) - b.at<uchar>(k+2,j+1));
+                s += abs (b.at<uchar>(k,j) - b.at<uchar>(k+2,j+2));
+            }
+
+            for(int k = i; k <= i + 2; ++k){
+                s += abs (b.at<uchar>(k, j-2) - b.at<uchar>(k-2,j));
+                s += abs (b.at<uchar>(k,j-1) - b.at<uchar>(k-2,j+1));
+                s += abs (b.at<uchar>(k,j) - b.at<uchar>(k-2,j+2));
+            }
+
+            s /= 36;
+
+            double s_vert(0), s_hor(0), s_maindig(0), s_subdig(0), s_max(0);
+
+            for(int k = -1; k <= 1; ++k){
+                s_vert += abs(b.at<uchar>(i-1,j+k) - b.at<uchar>(i+1,j+k))*GK;
+            }
+
+            for(int k = -1; k <= 1; ++k){
+                s_hor += abs(b.at<uchar>(i+k,j-1) - b.at<uchar>(i+k,j+1))*GK;
+            }
+
+            for(int k = 0; k <= 2; ++k){
+                s_maindig += abs(b.at<uchar>(i-2+k,j+k) - b.at<uchar>(i+k,j-2+1));
+            }
+
+            for(int k = 0; k <= 2; ++k){
+                s_subdig += abs(b.at<uchar>(i-2+k,j-k) - b.at<uchar>(i+k,j+2-k));
+            }
+
+
+            if ( s_vert > s_hor && s_vert > s_maindig && s_vert > s_subdig){
+                s_max = s_vert;
+                if ((s_max > s*3*AV) && (s_max > G_AV * av_diff_vert)) {
+                    if (s_hor < s_maindig && s_hor < s_subdig)
+                        for(int c =0 ; c < 1; ++c){
+                            output_img.at<uchar>(i,j+c) = 254;
+                        }
+                }
+            } else if(s_hor >= s_vert && s_hor >= s_maindig && s_hor >= s_subdig){
+                       s_max = s_hor;
+                       if ((s_max > s*3*AV)&& (s_max > G_AV * av_diff_hor)) {
+                           if (s_vert < s_maindig && s_vert < s_subdig)
+                               for(int c =0 ; c < 1; ++c){
+                                   output_img.at<uchar>(i+c,j) = 254;
+                               }
+                       }
+            } else if(s_maindig > s_hor && s_maindig > s_vert && s_maindig > s_subdig){
+                       s_max = s_maindig;
+                       if ((s_max > s*3*AV)&& (s_max > G_AV * av_diff_maindig)) {
+                           if(s_vert > s_subdig && s_hor > s_subdig)
+                               for(int c =0 ; c < 1; ++c){
+                                   output_img.at<uchar>(i+c,j+c) = 254;
+                               }
+                       }
+            } else {
+                s_max = s_subdig;
+                if ((s_max > s*3*AV) && (s_max > G_AV * av_diff_subdig)) {
+                    if(s_vert > s_maindig && s_hor > s_maindig)
+                        for(int c =0 ; c < 1; ++c){
+                            output_img.at<uchar>(i+c,j-c) = 254;
+                        }
+                }
+              }
+        }
+    }
+
+return output_img;
+}
+
+
+
+
+cv::Mat medianFilter_8UC1 (const cv::Mat &input_img){         // Против изолированых белых точек в контуре
+
+    cv::Mat output_img = input_img.clone();
+
+    output_img.convertTo(output_img, CV_8UC1);
+
+    int max = 5;
+    int h = input_img.rows;
+    int w = input_img.cols;
+
+    for (int i = 1; i < h-1; ++i){
+        for (int j = 1; j < w-1; ++j){
+            if (output_img.at<uchar>(i,j) >= max){
+                if(!(( output_img.at<uchar>(i,j-1)  >= max &&
+                       output_img.at<uchar>(i,j+1)  >= max ) ||
+                    ( output_img.at<uchar>(i-1,j)   >= max &&
+                      output_img.at<uchar>(i+1,j)   >= max ) ||
+                    ( output_img.at<uchar>(i-1,j-1) >= max &&
+                      output_img.at<uchar>(i+1,j+1) >= max ) ||
+                    ( output_img.at<uchar>(i+1,j-1) >= max &&
+                      output_img.at<uchar>(i-1,j+1) >= max) )){
+
+                    output_img.at<uchar>(i,j) = 25;
+                }
+            }
+        }
+    }
+
+return output_img;
+}
