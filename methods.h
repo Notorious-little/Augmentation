dc01038eaa0a6c113075d4e6bd4c6f1edef1bdd2
Notorious-little <72048638+Notorious-little@users.noreply.github.com>
2020-10-11 15:57:08 +0200

    Update methods.h

--- methods.h
+++ methods.h
@@ -7,14 +7,18 @@
 #include <cstdio>
 #include <cmath>
 
-cv::Mat autoContrast(const cv::Mat img, const int quantil);
-
-cv::Mat localContours(const cv::Mat input_img);
+cv::Mat autoContrast(const cv::Mat &img, const int quantil);
 
 void slidingNormalMatrixCompute(double* elements, const int MatrixSizeParam);
 
-cv::Mat blurByGaussMatrix(const cv::Mat input_img, const int blurpower);
-
-cv::Mat gaussNoize( const cv::Mat input_img, const int noize_range);
+cv::Mat blurByGaussMatrix(const cv::Mat &input_img, const int blurpower);
 
 double exp_rand(void);
+
+cv::Mat gaussNoize( const cv::Mat &input_img, const int noize_range);
+
+cv::Mat salt_paperNoize( const cv::Mat &input_img, const int noize_range);
+
+cv::Mat localContours(const cv::Mat &input_img);
+
+cv::Mat medianFilter_8UC1 (const cv::Mat &input_img);
